40022d9db09dfb4f6bda88753ba0a5665bb72131
vinay kumar <vk1230460@gmail.com>
2025-06-20 19:35:09 +0530

    20-06-2025

--- app/src/main/java/com/vinay/chatapp/Models/Users.java
+++ app/src/main/java/com/vinay/chatapp/Models/Users.java
@@ -11,23 +11,18 @@ public class Users
     String profilepic;
     String coverphoto;
     int followercount;
-
     public int getFollowercount() {
         return followercount;
     }
-
     public void setFollowercount(int followercount) {
         this.followercount = followercount;
     }
-
     public String getCoverphoto() {
         return coverphoto;
     }
-
     public void setCoverphoto(String coverphoto) {
         this.coverphoto = coverphoto;
     }
-
     public Users(String uid, String username, String mobile, String email, String password) {
         this.uid = uid;
         this.username = username;
@@ -36,59 +31,45 @@ public class Users
         this.password = password;
     }
     public Users(){}
-
     public String getUid() {
         return uid;
     }
-
     public void setUid(String uid) {
         this.uid = uid;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getMobile() {
         return mobile;
     }
-
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getLastmessage() {
         return lastmessage;
     }
-
     public void setLastmessage(String lastmessage) {
         this.lastmessage = lastmessage;
     }
-
     public String getProfilepic() {
         return profilepic;
     }
-
     public void setProfilepic(String profilepic) {
         this.profilepic = profilepic;
     }
