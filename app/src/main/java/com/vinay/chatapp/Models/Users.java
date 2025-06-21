fc4ed6a4bf4e17c5c01c9d2132611fb1d09ef009
vinay kumar <vk1230460@gmail.com>
2025-06-21 22:35:39 +0530

    21-06-2025

--- app/src/main/java/com/vinay/chatapp/Models/Users.java
+++ app/src/main/java/com/vinay/chatapp/Models/Users.java
@@ -34,38 +34,14 @@ public class Users
     public String getUid() {
         return uid;
     }
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
     public String getUsername() {
         return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
     }
     public String getMobile() {
         return mobile;
     }
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
     public String getEmail() {
         return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getLastmessage() {
-        return lastmessage;
-    }
-    public void setLastmessage(String lastmessage) {
-        this.lastmessage = lastmessage;
     }
     public String getProfilepic() {
         return profilepic;
